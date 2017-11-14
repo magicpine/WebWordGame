019c76edbbda9294fdcacf2464ef084d87970544
Christian Bradford <c00223037@itcarlow.ie>
2017-11-14 16:49:12 +0000

    Finished the top ten page.  Changed the messages(for the loser page)  to use flash. Also added in Markup to use markup langauge in the error messages.  Added in a redirect for the top ten page so you don't keep resubmmiting

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for, Markup, flash
 from datetime import datetime
 
 import word_utils
@@ -37,10 +37,10 @@ def processWords():
 		if len(wordList) != 7:
 			winner = False
 			if len(wordList) < 7:
-				tmp = "You didn't type in enough words.  Words Detected: {0} Words needed: 7".format(len(wordList))
+				tmp = Markup("You didn't type in enough words.  Words Detected: <b>{0}</b> Words needed: <b>7</b>".format(len(wordList)))
 			else:
-				tmp = 'You typed in too many words.  Words Detected: {0} Words needed: 7'.format(len(wordList))
-			errors.append(tmp)
+				tmp = Markup('You typed in too many words.  Words Detected: <b>{0}</b> Words needed: <b>7</b>'.format(len(wordList)))
+			flash(tmp)
 		#Rule One
 		for words in wordList:
 			wrongLettersList = []
@@ -53,15 +53,22 @@ def processWords():
 					wrongLettersList.append(letters[0])	
 			if (isError):
 				isError = False
-				tmp = 'You used these wrong letters [{0}] in this word: {1}.'.format(','.join(wrongLettersList), words)
-				errors.append(tmp)
+				tmp = Markup('You used these wrong letters <b>[{0}]</b> in this word: <b>{1}</b>.'.format(','.join(wrongLettersList), words))
+				flash(tmp)
 		#Rule Two
+		spellCheck = wordgame.check_spellings(wordList)
+		for words in spellCheck:
+			if words[1] == False:
+				winner = False
+				tmp = Markup('The word: <b>{0}</b> is not spelled correctly'.format(words[0]))
+				flash(tmp)
+		#Rule Three/
 		length = word_utils.check_size(wordList)
 		for words in length:
 			if words[1] == False:
 				winner = False
-				tmp = 'The word: {0} is not three or more characters long'.format(words[0])
-				errors.append(tmp)
+				tmp = Markup('The word: <b>{0}</b> is not three or more characters long'.format(words[0]))
+				flash(tmp)
 		#Rule Four
 		if word_utils.duplicates(wordList):
 			winner = False
@@ -69,30 +76,37 @@ def processWords():
 			for words in wordList:
 				wordListCopy.remove(words)
 				if words in wordListCopy:
-					tmp = 'The word: {0} was duplicated'.format(words)
-					errors.append(tmp)
+					tmp = Markup('The word: <b>{0}</b> was duplicated'.format(words))
+					flash(tmp)
 		#Rule Five
 		sameSourceWord = []
 		sameSourceWord = word_utils.check_not_sourceword(wordList, session['sourceWord'])
 		if len(sameSourceWord) != 0:
 			winner = False
-			tmp = 'You tried to use the source word {0} time(s)'.format(len(sameSourceWord))
-			errors.append(tmp)
+			tmp = Markup('You tried to use the source word <b>{0}</b> time(s)'.format(len(sameSourceWord)))
+			flash(tmp)
 		#End Rules
 		if winner:
 			return render_template('winner.html',
 									the_title='Winner!',
 									timeTook = session['timeTook'])
 		return render_template('loser.html',
-								the_title='Loser',
-								errors = errors)
+								the_title='Loser')
 
 
-@app.route('/processtime', methods=['POST'])
+@app.route('/processtime', methods=['POST', 'GET'])
 def topten():
-	pass
-
-
+	if request.method == 'POST':
+		session['name'] = request.form['name'].strip()
+		data_utils.add_to_scores(session['name'], session['timeTook'])
+		return redirect(url_for('topten'))
+	elif request.method == 'GET':
+		leaderBoard = data_utils.get_sorted_leaderboard()
+		return render_template('processtime.html',
+								the_title='Top Ten for Word Game',
+								board = leaderBoard,
+								pos = leaderBoard.index((session['timeTook'], session['name']))+1,
+								total = len(leaderBoard))
 
 if __name__ == '__main__':
 	app.secret_key = 'itsmybirthdaythatsthepassword'
